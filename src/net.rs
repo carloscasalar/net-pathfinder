6e05361dc3bf2aeea8af5e51c03ead4d0064b691
Carlos Castillo <castillo.st@gmail.com>
2018-07-30 20:20:21 +0200

    Implement find path in a net of two connected points happy path

--- src/net.rs
+++ src/net.rs
@@ -5,20 +5,26 @@ pub struct Net<T: Point> {
     pub nodes: Vec<Node<T>>
 }
 
-impl<T: Point> Net<T> {
+impl<'a, T: Point> Net<T> {
+    pub fn find_paths(&self, from: &'a T, to: &'a T) -> Result<Vec<Vec<&'a T>>, NetErrors> {
+        self.find_node_or_throws(to)?;
+        let node_from = self.find_node_or_throws(from)?;
 
-    fn assert_exists(&self, point: &T)->Result<(),NetErrors>{
-       match self.nodes.iter().find(|p| p.point.is(point)) {
-           Some(_) => Ok(()),
-           None => Err(NetErrors::PointNotFound(point.id().to_string()))
-       }
+        let mut paths = Vec::new();
+        if node_from.is_connected_to(to){
+            let path = vec!(from, to);
+            paths.push(path);
+        }
+
+        Ok(paths)
     }
 
-    pub fn find_paths(&self, from: &T, to: &T) -> Result<Vec<Vec<T>>, NetErrors> {
-        self.assert_exists(from)?;
-        self.assert_exists(to)?;
-
-        Ok(Vec::new())
+    fn find_node_or_throws(&self, point: &T) -> Result<&Node<T>, NetErrors> {
+        let node_point = self.nodes.iter().find(|node| node.point.is(point));
+        match node_point {
+            Some(ref node) => Ok(node),
+            None => Err(NetErrors::PointNotFound(point.id().to_string()))
+        }
     }
 }
 
@@ -44,7 +50,7 @@ mod test {
     const B: char = 'B';
     const C: char = 'C';
 
-    #[derive(Copy,Clone)]
+    #[derive(Copy, Clone, PartialEq, Debug)]
     struct SimplePoint {
         name: char
     }
@@ -72,9 +78,9 @@ mod test {
             nodes: vec![node_a, node_b]
         };
 
-        let path = a_b_net.find_paths(&point_c, &point_a);
+        let paths = a_b_net.find_paths(&point_c, &point_a);
 
-        assert!(path.is_err(), "Should not be able to find the path from a point that does not exists in the net");
+        assert!(paths.is_err(), "Should not be able to find the path from a point that does not exists in the net");
     }
 
     // Given this net:
@@ -92,19 +98,48 @@ mod test {
             nodes: vec![node_a, node_b]
         };
 
-        let path = a_b_net.find_paths(&point_a, &point_c);
+        let paths = a_b_net.find_paths(&point_a, &point_c);
 
-        assert!(path.is_err(), "Should not be able to find the path to a point that does not exists in the net");
+        assert!(paths.is_err(), "Should not be able to find the path to a point that does not exists in the net");
+    }
+
+    // Given this net:
+    // A - B
+    #[test]
+    fn in_a_b_net_find_path_should_find_a_path_from_a_to_b() {
+        let point_a = simple_point(A);
+        let point_b = simple_point(B);
+
+        let node_a = node(point_a, point_b);
+        let node_b = node(point_b, point_a);
+
+        let a_b_net: Net<SimplePoint> = Net {
+            nodes: vec![node_a, node_b]
+        };
+
+        let paths = a_b_net.find_paths(&point_a, &point_b)
+            .expect("Unexpected error while finding path");
+
+        assert_eq!(paths.len(), 1, "Should find one path");
+
+        let path_from_a_to_b = &paths[0];
+
+        let first_point = &path_from_a_to_b[0];
+
+        let last_point = &path_from_a_to_b[1];
+
+        assert!(first_point.is(&point_a), "Path should begin with A point");
+        assert!(&last_point.is(&point_b), "Path should begin with A point");
     }
 
     fn simple_point(name: char) -> SimplePoint {
         SimplePoint { name: name.clone() }
     }
 
-    fn node(from: SimplePoint, to: SimplePoint) -> Node<SimplePoint>{
+    fn node(from: SimplePoint, to: SimplePoint) -> Node<SimplePoint> {
         Node {
             point: from.clone(),
-            connections: vec![Connection{ to: to.clone()}]
+            connections: vec![Connection { to: to.clone() }],
         }
     }
 }
